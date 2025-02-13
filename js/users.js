b2daaaab469c5fbf8c52128ca9534d5bfc67f033
valeria013 <valeriagoncalves0401@gmail.com>
2025-02-13 07:44:18 -0300

    salvando informacoes no backend

---
+++ js/users.js
@@ -0,0 +1,62 @@
+// FUNÇÃO PARA ADICIONAR USUÁRIO
+function adicionarUsuario() {
+    const campoNome = document.querySelector("#nome").value;
+    const campoEmail = document.querySelector("#email").value;
+    const campoSenha = document.querySelector("#senha").value;
+  
+    const usuario = {
+      name: campoNome,
+      email: campoEmail,
+      password: campoSenha,
+    };
+  
+    fetch('https://projeto-integrador-planeje-e-estude.onrender.com/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(usuario)
+    }).then(response => {
+      response.json().then(data => {
+        console.log("Usuário cadastrado com sucesso: ", data)
+        window.location.href = 'principal.html'
+      })
+    }).catch(error => {
+      console.log("Erro ao cadastrar usuário: ", error)
+    })
+  
+  }
+  
+  //FUNÇÃO PARA FAZER LOGIN
+  async function loginUsuario() {
+    const campoEmail = document.querySelector("#email").value;
+    const campoSenha = document.querySelector("#senha").value;
+  
+    const usuario = {
+      email: campoEmail,
+      password: campoSenha
+    };
+    try {
+      const resposta = await fetch('https://projeto-integrador-planeje-e-estude.onrender.com/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(usuario),
+      });
+  
+      if (!resposta.ok) {
+        throw new Error('Email ou senha inválidos');
+      }
+  
+      const { user, token } = await resposta.json();
+  
+      localStorage.setItem('token', token);
+  
+      // Redireciona para a página inicial
+      window.location.href = 'principal.html';
+    } catch (error) {
+      console.error('Erro no login:', error);
+      alert('Erro ao realizar login. Tente novamente mais tarde.');
+    }
+  }
